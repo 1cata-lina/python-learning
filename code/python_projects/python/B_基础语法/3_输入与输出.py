b7313c44ed201a297c1d3c4ad128c9456b2c8493
1cata-lina <2950976865@qq.com>
2026-09-23 00:55:34 +0800

    python复习笔记整理

---
+++ code/python_projects/python/B_基础语法/3_输入与输出.py
@@ -0,0 +1,16 @@
+'''
+输入与输出
+print（）是输出函数
+input（）是输入函数：所有经过input都是字符串
+
+print（sep，end）
+sep：多个内容的分隔符号
+end：结尾默认换行，可以修改
+'''
+print("hello",end="")
+print("world",end="\t")#helloworld 这是输出结果
+print("helloworld")
+'''
+input（）接收的数据默认是字符串
+
+'''
